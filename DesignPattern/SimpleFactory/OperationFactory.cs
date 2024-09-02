ef0bafdf5cccd6c162893a277cd219afcff9bfa4
songtaojie <stjworkemail@163.com>
2024-09-02 22:23:28 +0800

    添加简单工厂示例

---
+++ DesignPattern/SimpleFactory/OperationFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class OperationFactory
+    {
+        public static Operation CreateOperation(string? operate)
+        {
+            switch (operate)
+            {
+                case "+":
+                    return new OperationAdd();
+                case "-":
+                    return new OperationSub();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
--- DesignPattern/SimpleFactory/Program.cs
+++ DesignPattern/SimpleFactory/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("请输入数字A：");
+                var stringNumberA = Console.ReadLine();
+                Console.WriteLine("请输入运算符(+、-，*，/)：");
+                var operate = Console.ReadLine();
+                Console.WriteLine("请输入数字B：");
+                var stringNumberB = Console.ReadLine();
+                var operation = OperationFactory.CreateOperation(operate);
+                operation.NumberA = Convert.ToDouble(stringNumberA);
+                operation.NumberB = Convert.ToDouble(stringNumberB);
+                var result =operation.GetResult();
+                Console.WriteLine("运算结果为：" + result);
+                Console.ReadLine();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"输入参数有误：{ex.Message}");
+            }
         }
     }
 }
